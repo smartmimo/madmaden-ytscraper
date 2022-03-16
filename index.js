70f3d11329ec99e8b6a22dcd0db6a377bf24b169
smartmimo <el.attaoui.mohamed@gmail.com>
2022-03-16 18:45:26 +0100

    added stream and metadata

--- index.js
+++ index.js
@@ -6,6 +6,59 @@ const app = express();
 
 app.use(bodyParser.json());
 
+function stream(uri, opt) {
+    opt = {
+        ...opt,
+        videoFormat: 'mp4',
+        quality: 'highest',
+        audioFormat: 'mp3',
+        filter(format) {
+            return format.container === opt.videoFormat && format.audioBitrate
+        }
+    }
+
+    const video = ytdl(uri, opt)
+    const { file, audioFormat } = opt
+    const stream = file ? fs.createWriteStream(file) : new PassThrough()
+    const ffmpeg = new FFmpeg(video)
+
+    process.nextTick(() => {
+        const output = ffmpeg.format(audioFormat).pipe(stream)
+
+        ffmpeg.once('error', error => stream.emit('error', error))
+        output.once('error', error => {
+            video.end()
+            stream.emit('error', error)
+        })
+    })
+
+    stream.video = video
+    stream.ffmpeg = ffmpeg
+
+    return stream
+}
+
+app.get("/getSong", async (req, res) => {
+    const url = `https://youtu.be/${req.query.id}`;
+    const song = await ytdl.getInfo(url).catch(e => e)
+    res.json({
+        url: song.videoDetails.videoId,
+        title: (song.videoDetails.media && song.videoDetails.media.song) ? song.videoDetails.media.song : song.videoDetails.title,
+        artist: song.videoDetails.media ? song.videoDetails.media.artist || "" : "",
+        seconds: parseInt(song.videoDetails.lengthSeconds),
+        thumbnail: song.videoDetails.thumbnail.thumbnails[0]
+    })
+})
+
+app.get("/getStream", async (req, res) => {
+    const url = `https://youtu.be/${req.query.id}`;
+    
+    for await (const chunk of stream(url)) {
+        res.write(chunk)
+    }
+    res.end()
+})
+
 app.post("/search", async (req, res)=>{
     if(!req.body.query) return res.status(400).end("Missing params.");
     const result = await axios("https://www.youtube.com/results?search_query=salam");
